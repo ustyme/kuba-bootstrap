f9a7930913a28b8aef36ccd4233adaaa603c6010
Taylor Coleman <taylor@ustyme.com>
2016-03-11 15:02:51 -0800

    Differential swipe sensitivity for books vs other swipes

--- js/general/kb-swipe.js
+++ js/general/kb-swipe.js
@@ -8,7 +8,7 @@ var swipeFlag = false;
         upEventS = 'touchend ' + 'mouseup ';
 
     $.fn.swipe = function(){
-        var args = arguments[0] || { swipeTime: 900, swipeXleft: 275, swipeXright: 200, left: left, right: right, leftProp: null, rightProp: null };
+        var args = arguments[0] || { swipeTime: 900, swipeX: 200, left: left, right: right, leftProp: null, rightProp: null };
 
         var startX = 0,
             startTime = 0;
@@ -18,7 +18,7 @@ var swipeFlag = false;
                 startX = e.originalEvent.touches ? e.originalEvent.touches[0].pageX : e.pageX;
             })
             .on(upEventS, function(e){
-                
+
                 swipeFlag = false;
 
                 startTime = 0;
@@ -28,28 +28,31 @@ var swipeFlag = false;
                 var currentX = e.originalEvent.touches ? e.originalEvent.touches[0].pageX : e.pageX,
                     currentDistance = (startX === 0) ? 0 : Math.abs(currentX - startX),
                     currentTime = e.timeStamp;
-                if (startTime !== 0 && currentTime - startTime < args.swipeTime ) {
-
-                    if (currentDistance > args.swipeXleft && currentX < startX) {
+                if (startTime !== 0 && currentTime - startTime < args.swipeTime && currentDistance > args.swipeX) {
+                    if (currentX < startX) {
                         e.preventDefault();
-                        if (args.leftProp != null) {
+
+                        swipeFlag = true;
+
+                        if(args.leftProp != null){
                             args.left(args.leftProp);
-                        } else {
+                        }else{
                             args.left();
                         }
-                        startTime = 0;
-                        startX = 0;
                     }
-                    else if (currentDistance > args.swipeXright && currentX > startX) {
+                    if (currentX > startX) {
                         e.preventDefault();
-                        if (args.rightProp != null) {
+
+                        swipeFlag = true;
+
+                        if(args.rightProp != null){
                             args.right(args.rightProp);
-                        } else {
+                        }else{
                             args.right();
                         }
-                        startTime = 0;
-                        startX = 0;
                     }
+                    startTime = 0;
+                    startX = 0;
                 }
             });
 
